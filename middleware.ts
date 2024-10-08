a44b2ccad1f29774186af4e3265892109fa81e93
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-10-09 01:06:58 +0300

    middleware55

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/webhooks(.*)",
+    "/api/webhooks",
     "/question/:id",
     "/tags",
     "/tags/:id",
