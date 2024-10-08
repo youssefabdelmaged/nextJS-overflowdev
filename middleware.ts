cb3af9c9f24640efb375f7472da4c00a3f75763a
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-10-08 23:16:39 +0300

    m

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,19 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-// Define public routes, including "/api/webhook"
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api/webhook",  // Make it public
-]);
-
-export default clerkMiddleware((auth, request) => {
-  const pathname = request.nextUrl.pathname;
-
-  // Ignore authentication and any processing for /api/webhook
-  if (pathname === "/api/webhook") {
-    return; // Completely bypass authentication and middleware logic
-  }
-
-  // Apply authentication for non-public routes
-  if (!isPublicRoute(pathname)) {
-    auth().protect();
-  }
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and static files
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Ensure middleware runs for API and TRPC routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
