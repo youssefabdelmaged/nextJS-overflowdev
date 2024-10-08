73bac267f00834c7b4e40359740a453f270df81a
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-10-09 01:12:30 +0300

    c

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,15 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/webhooks",
-    "/question/:id",
-    "/tags",
-    "/tags/:id",
-    "/profile/:id",
-    "/community",
-    "/jobs",
-  ],
-  ignoredRoutes: ["/api/webhooks", "/api/chatgpt"],
-});
+const isProtectedRoute = createRouteMatcher([
+'/',
+  '/api/webhooks(.*)'
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
-// 
+
+}
