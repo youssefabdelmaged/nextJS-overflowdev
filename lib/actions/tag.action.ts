71471f096384281d1131a182792830533ca4b0b8
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-10-10 09:42:05 +0300

    tags page

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,7 +1,8 @@
 "use server";
 import User from "@/database/user.model";
 import { connectToDatabase } from "./mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types.d";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types.d";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -15,8 +16,22 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
     return [
       { _id: "1", name: "tag1" },
       { _id: "2", name: "tag2" },
-      { _id: "3", name: "tag3" },
+      
     ];
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
 
