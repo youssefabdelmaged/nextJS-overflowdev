fe38f825f8fc451a90b50d1011bdb440064a827a
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-10-13 22:49:15 +0300

    voting for questions & answers

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { getTimeStamp } from "@/lib/utils";
 import ParseHtml from "./ParseHtml";
+import Votes from "./Votes";
 
 interface Props {
   questionId: string;
@@ -34,7 +35,7 @@ const AllAnswers = async ({
         {result.answers.map((answer) => (
           <article key={answer._id} className="light-border border-b py-10">
             <div className=" flex items-center justify-between  ">
-              <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+              <div className="mb-8 flex flex-col-reverse justify-between  gap-5 sm:flex-row sm:items-center sm:gap-2  w-full">
                 <Link
                   href={`/profile/${answer.author.clerkId}`}
                   className="flex flex-1 items-start gap-1 sm:items-center"
@@ -58,11 +59,19 @@ const AllAnswers = async ({
                   </div>
                 </Link>
                 <div className="flex justify-end">
-                    Voting
+                  <Votes
+                    type="Answer"
+                    itemId={JSON.stringify(answer._id)}
+                    userId={JSON.stringify(userId)}
+                    upvotes={answer.upvotes.length}
+                    hasupVoted={answer.upvotes.includes(userId)}
+                    downvotes={answer.downvotes.length}
+                    hasdownVoted={answer.downvotes.includes(userId)}
+                  />
                 </div>
               </div>
             </div>
-              <ParseHtml data={answer.content}/>
+            <ParseHtml data={answer.content} />
           </article>
         ))}
       </div>
--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,9 +1,144 @@
-import React from 'react'
-
-const Votes = () => {
-  return (
-    <div>Votes</div>
-  )
+"use client";
+import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import {
+  downvoteQuestion,
+  upvoteQuestion,
+} from "@/lib/actions/question.action";
+import { formatNumber } from "@/lib/utils";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
 }
 
-export default Votes
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved,
+}: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleSaved = () => {};
+
+  const handleVote = async (action: string) => {
+    if (!userId) {
+      return;
+    }
+    if (action === "upvote") {
+      if (type === "Question") {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        await upvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      }
+      return;
+    }
+
+    if (action === "downvote") {
+      if (type === "Question") {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        await downvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      }
+      return;
+    }
+  };
+
+  return (
+    <div className="flex gap-5">
+      <div className=" flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasupVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="upvote"
+            className="cursor-pointer "
+            onClick={() => handleVote("upvote")}
+          />
+          <div className="background-light700_dark400 flex-center p-1 min-w-[18px]  ">
+            <p className="subtle-medium text-dark400_light900">
+              {formatNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasdownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="upvote"
+            className="cursor-pointer "
+            onClick={() => handleVote("downvote")}
+          />
+          <div className="background-light700_dark400 flex-center p-1 min-w-[18px]  ">
+            <p className="subtle-medium text-dark400_light900">
+              {formatNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+      {type === "Question" && (
+        <Image
+          src={
+            hasSaved
+              ? "/assets/icons/star-filled.svg"
+              : "/assets/icons/star-red.svg"
+          }
+          width={18}
+          height={18}
+          alt="star"
+          className="cursor-pointer "
+          onClick={handleSaved}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Votes;
